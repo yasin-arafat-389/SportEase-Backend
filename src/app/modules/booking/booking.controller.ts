0ead6c0dc6970f364d6c49c23936159baf133c73
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-06-12 09:22:59 +0600

    Created services for booking a facility

--- src/app/modules/booking/booking.controller.ts
+++ src/app/modules/booking/booking.controller.ts
@@ -4,7 +4,7 @@ import { BookingServices } from './booking.service';
 
 const createBooking: RequestHandler = async (req, res, next) => {
   try {
-    const result = await BookingServices.createBooking(req.body);
+    const result = await BookingServices.createBooking(req.body, req.user);
 
     sendResponse(res, result, 'Booking created succesfully');
   } catch (error) {
@@ -12,6 +12,39 @@ const createBooking: RequestHandler = async (req, res, next) => {
   }
 };
 
+const viewAllBookings: RequestHandler = async (req, res, next) => {
+  try {
+    const result = await BookingServices.viewAllBookings();
+
+    sendResponse(res, result, 'Bookings retrieved succesfully');
+  } catch (error) {
+    next(error);
+  }
+};
+
+const viewAllBookingsByUser: RequestHandler = async (req, res, next) => {
+  try {
+    const result = await BookingServices.viewAllBookingsByUser(req.user);
+
+    sendResponse(res, result, 'Bookings retrieved succesfully');
+  } catch (error) {
+    next(error);
+  }
+};
+
+const cancelBooking: RequestHandler = async (req, res, next) => {
+  try {
+    const result = await BookingServices.cancelBooking(req.params.id);
+
+    sendResponse(res, result, 'Booking cancelled succesfully');
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const BookingControllers = {
   createBooking,
+  viewAllBookings,
+  viewAllBookingsByUser,
+  cancelBooking,
 };
