bcaf1c854d0287ce50a66cb9fb1a150519177d6f
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-08-27 22:16:40 +0600

    Fixed a bug with generating static html page

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,12 @@ const app: Application = express();
 //parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://sport-ease-client.web.app'],
+    credentials: true,
+  }),
+);
 
 // application routes
 app.use('/api', router);
